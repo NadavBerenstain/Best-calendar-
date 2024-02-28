63a68ce15e80d4927e8dcef8bb909002da0d39cb
Nadav <nadavberenstain@gmail.com>
2024-02-28 18:20:05 +0100

    6:19 28/2

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState()
+
+  return (
+    <>
+      <h1>HI</h1>
+    </>
+  )
+}
+
+export default App
