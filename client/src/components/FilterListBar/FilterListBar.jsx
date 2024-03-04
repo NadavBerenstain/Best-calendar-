1867100778f9daac020d4e1c044aabf919cfbe8e
Nadav <nadavberenstain@gmail.com>
2024-03-04 20:22:05 +0100

    20:21 4.3

--- client/src/components/FilterListBar/FilterListBar.jsx
+++ client/src/components/FilterListBar/FilterListBar.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import "./FilterListBar.css";
 
-export default function FilterListBar({ eventsList, setEventsList, baseList }) {
+export default function FilterListBar({
+  eventsList,
+  setEventsList,
+  baseList,
+  themesList,
+  setThemesList,
+}) {
   const [inputVal, setInputVal] = useState(5);
   const [baseImportant, setBaseImportrant] = useState([]);
   const [importantClicked, setImportantClicked] = useState(false);
   const [fromDate, setFromDate] = useState("");
   const [untilDate, setUntilDate] = useState("");
+  ////////////////////////////////////////////////////////
+  const [selectedTheme, setSelectedTheme] = useState("");
 
   function sortImportants() {
     setFromDate("");
     setUntilDate("");
+    setSelectedTheme("");
     setEventsList(baseList.filter((event) => event.important === true));
     setBaseImportrant(baseList.filter((event) => event.important === true));
     setImportantClicked(true);
@@ -18,6 +27,7 @@ export default function FilterListBar({ eventsList, setEventsList, baseList }) {
   function showAllEvents() {
     setFromDate("");
     setUntilDate("");
+    setSelectedTheme("");
     setEventsList(baseList);
     setImportantClicked(false);
   }
@@ -45,48 +55,77 @@ export default function FilterListBar({ eventsList, setEventsList, baseList }) {
     setUntilDate(e.target.value);
     setEventsList(eventsList.filter((event) => event.date < e.target.value));
   }
-
+  function sortByTheme(theme) {
+    setEventsList(baseList.filter((event) => event.theme === theme));
+  }
+  /////////////////////////////////////////////////////////////////////////////////
+  function handleThemeSelection(e) {
+    const newTheme = e.target.value;
+    setSelectedTheme(newTheme);
+    sortByTheme(newTheme);
+  }
+  ///////////////////////////////////////////////////////////////////////////////////////
   return (
-    <div id="listBar">
-      <h3 id="barTitle">filter events:</h3>
-
-      <div id="barFunc">
-        <button className="filtterButtons" onClick={sortImportants}>
-          Iportants
-        </button>
-
-        <label>from</label>
-        <input
-          name="from"
-          type="datetime-local"
-          id="fromDate"
-          value={fromDate}
-          onChange={(e) => sortFrom(e)}
-        />
-        <label>until</label>
-        <input
-          name="until"
-          type="datetime-local"
-          id="untilDate"
-          value={untilDate}
-          onChange={(e) => sortUntil(e)}
-        />
-
-        <div>
-          <label>Display amount:</label>
+    // <div id="listBar">
+    <div id="barFunc">
+      <button className="filtterButtons" onClick={sortImportants}>
+        Iportants
+      </button>
+      <div id="timeRange">
+        <div id="from">
+          <label>from: </label>
           <input
-            className="numOfEvents"
-            value={inputVal}
-            onChange={handleInputChange}
-            type="number"
-            min={1}
-            max={importantClicked ? baseImportant.length : baseList.length}
+            name="from"
+            type="datetime-local"
+            id="fromDate"
+            value={fromDate}
+            onChange={(e) => sortFrom(e)}
           />
         </div>
-        <button className="filtterButtons" onClick={showAllEvents}>
-          Show all
-        </button>
+        <div id="until">
+          <label>until: </label>
+          <input
+            name="until"
+            type="datetime-local"
+            id="untilDate"
+            value={untilDate}
+            onChange={(e) => sortUntil(e)}
+          />
+        </div>
       </div>
+
+      <div id="amout">
+        <label>Display amount:</label>
+        <input
+          id="amoutInput"
+          className="numOfEvents"
+          value={inputVal}
+          onChange={handleInputChange}
+          type="number"
+          min={1}
+          max={importantClicked ? baseImportant.length : baseList.length}
+        />
+      </div>
+      <div id="findTheme">
+        <label> Find theme:</label>
+        <select
+          id="themesDropdown"
+          value={selectedTheme}
+          onChange={handleThemeSelection}
+        >
+          <option value="" disabled>
+            Select a theme...
+          </option>
+          {themesList.map((theme, index) => (
+            <option key={index} value={theme}>
+              {theme}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button className="filtterButtons" onClick={showAllEvents}>
+        Show all
+      </button>
     </div>
   );
 }
